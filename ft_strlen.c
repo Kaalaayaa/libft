04e3f35f5caeb483eac5060970c73ba6043ed62d
Kalaya Chatelain <kchatela@student.42berlin.de>
2024-11-12 17:33:51 +0100

    created makefile libft.h and ft_atoi and ft_strlen

---
+++ ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchatela <kchatela@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 12:09:20 by kchatela          #+#    #+#             */
+/*   Updated: 2024/11/12 17:23:51 by kchatela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlen(const char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        i++;
+    }
+    return (i);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char str[] = "Kalaya";
+// 	printf("%d", ft_strlen(str));
+// }
---
+++ libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int ft_atoi(const char *str);
+size_t ft_strlen(const char *str);
+
+#endif
