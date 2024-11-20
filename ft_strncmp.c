c5c974e7fb3d93374290d2820d9e68ad78468b32
Kalaya Chatelain <kchatela@student.42berlin.de>
2024-11-20 18:33:32 +0100

    ft_strncmp added

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,20 +6,51 @@
 /*   By: kchatela <kchatela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 17:51:42 by kchatela          #+#    #+#             */
-/*   Updated: 2024/11/20 18:02:55 by kchatela         ###   ########.fr       */
+/*   Updated: 2024/11/20 18:33:06 by kchatela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int strcmp(const char *s1, const char *s2, size_t n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+    size_t i;
+
+    i = 0;
+    while (s1[i] && s2[i] && i < n)
+    {
+        if (s1[i] != s2[i])
+        {
+            return ((unsigned char) s1[i] - (unsigned char)s2[i]);
+        }
+        i++;
+    }
+
+    if (i < n)
+    {
+        return ((unsigned char) s1[i] - (unsigned char)s2[i]);
+    }
     
+    return (0);
 }
 
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+int	main(void)
 {
-   char s1[] = "Kalaya";
+	int		n;
+	char	string1[50];
+	char	string2[50];
+
+	n = 9;
+	strcpy(string1, "1234567890");
+	strcpy(string2, "1234567");
+
+	printf(" \n");
+	printf("== TEST ==\nS1: %s\nS2: %s\nn:%d\n", string1, string2, n);
+	printf("Result strcmp(): %d\n", strncmp(string1, string2, n));
+	printf("Result 'my' ft_strcmp(): %d\n", ft_strncmp(string1, string2, n));
+
+	return (0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kchatela <kchatela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:09:23 by kchatela          #+#    #+#             */
-/*   Updated: 2024/11/20 17:40:57 by kchatela         ###   ########.fr       */
+/*   Updated: 2024/11/20 18:07:50 by kchatela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,7 @@ size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
 int ft_toupper(int c);
 int ft_tolower(int c);
+char *strchr(const char *s, int c);
+char *strrchr(const char *s, int c);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 #endif
