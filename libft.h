fc3da301a8c22b8316bde5d458a23081cabea80f
Kalaya Chatelain <kchatela@student.42berlin.de>
2024-11-21 18:16:32 +0100

    libft.h modified

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kchatela <kchatela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:09:23 by kchatela          #+#    #+#             */
-/*   Updated: 2024/11/21 16:46:09 by kchatela         ###   ########.fr       */
+/*   Updated: 2024/11/21 17:36:11 by kchatela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 char *ft_strnstr(const char *haystack, const char *needle, size_t len);
+void *ft_calloc(size_t count, size_t size);
 
 #endif
